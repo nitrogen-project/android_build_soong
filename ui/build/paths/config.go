256075ae3a3f1d145b4238e877de65ecf78fea04
xyyx <xyyx@mail.ru>
2020-09-13 04:19:33 +0000

    soong: whitelist updates
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I2942ed2026382ec2d13b2f4eea37d32d94d7f338
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -75,6 +75,8 @@ func GetConfig(name string) PathConfig {
 
 var Configuration = map[string]PathConfig{
 	"bash":    Allowed,
+	"brotli":  Allowed,
+	"ccache":  Allowed,
 	"dd":      Allowed,
 	"diff":    Allowed,
 	"dlv":     Allowed,
@@ -90,6 +92,9 @@ var Configuration = map[string]PathConfig{
 	"lsof":    Allowed,
 	"openssl": Allowed,
 	"pstree":  Allowed,
+	"python3": Allowed,
+	"python3.6":  Allowed,
+	"repo":    Allowed,
 	"rsync":   Allowed,
 	"sh":      Allowed,
 	"stubby":  Allowed,
